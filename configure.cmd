d8eb0d9975b6cd7a524db049d43f4bedf0524352
Jan-Bruun-Andersen <jan_bruun_andersen@jabba.dk>
2015-12-05 20:24:28 +0100

    Moved token substitution to cl_token_subst.cmd.

--- configure.cmd
+++ configure.cmd
@@ -69,7 +69,7 @@
 
     goto :%action%
 :configure
-    call :subst install.cmd.tmpl install.cmd PROG_NAME=install DST_DIR="%prefix%"
+    call cl_token_subst install.cmd.tmpl install.cmd PROG_NAME=install DST_DIR="%prefix%"
     goto :exit
 :clean
     for %%F in (install.cmd) do (
@@ -77,50 +77,6 @@
 	if exist "%%F" del "%%F"
     )
     goto :exit
-goto :EOF
-
-:subst in-file out-file [token-assignment...]
-    setlocal enabledelayedexpansion
-
-    set "in_file=%~f1"  & shift
-    set "out_file=%~f1" & shift
-
-    for /L %%I in (1,1,6) do set "token%%I=" & set "value%%I="
-
-    if not "%~1" == "" set "token1=%1" & set "value1=%~2" & shift & shift
-    if not "%~1" == "" set "token2=%1" & set "value2=%~2" & shift & shift
-    if not "%~1" == "" set "token3=%1" & set "value3=%~2" & shift & shift
-    if not "%~1" == "" set "token4=%1" & set "value4=%~2" & shift & shift
-    if not "%~1" == "" set "token5=%1" & set "value5=%~2" & shift & shift
-    if not "%~1" == "" set "token6=%1" & set "value6=%~2" & shift & shift
-
-    if true == false (
-	if defined token1 echo token1="%token1%", value1="%value1%"
-	if defined token2 echo token2="%token2%", value2="%value2%"
-	if defined token3 echo token3="%token3%", value3="%value3%"
-	if defined token4 echo token4="%token4%", value4="%value4%"
-	if defined token5 echo token5="%token5%", value5="%value5%"
-	if defined token6 echo token6="%token6%", value6="%value6%"
-    )
-
-    if "%out_file%" == "%in_file%" (
-	echo Warning - Cowardly refuses to overwrite "%out_file%".
-	goto :error_exit
-    )
-
-    if exist "%out_file%" del "%out_file%"
-
-    for /F "usebackq delims=" %%I in (`type "%in_file%"`) do (
-	set "I=%%I"
-	if defined token1 set "I=!I:@%token1%@=%value1%!"
-	if defined token2 set "I=!I:@%token2%@=%value2%!"
-	if defined token3 set "I=!I:@%token3%@=%value3%!"
-	if defined token4 set "I=!I:@%token4%@=%value4%!"
-	if defined token5 set "I=!I:@%token5%@=%value5%!"
-	if defined token6 set "I=!I:@%token6%@=%value6%!"
-	echo.!I!>>"%out_file%"
-    )
-    endlocal
 goto :EOF
 
 rem .--------------------------------------------------------------------------
