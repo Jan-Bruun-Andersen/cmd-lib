892d3a7ae7934f510d3434895532db1e7ee7fa31
Jan-Bruun-Andersen <jan_bruun_andersen@jabba.dk>
2015-12-05 06:00:15 +0100

    Standard method to locate the library.

--- src/cmd/basename.cmd
+++ src/cmd/basename.cmd
@@ -12,20 +12,42 @@
 :: = SEE ALSO
 :: =   The built-in CALL command and the %~n1 parameter substitution syntax.
 
-    setlocal
+:: @author Jan Bruun Andersen
+:: @version @(#) Version: 2015-12-05
 
-    if "%~1" == "/?"call %~p0\cmd-lib.lib\cl_help  %~f0 & goto error_exit
-    if "%~1" == ""  call %~p0\cmd-lib.lib\cl_usage %~f0 & goto error_exit
-
-    call %~p0\cmd-lib.lib\cl_%0 %*
-    if defined _basename (echo.%_basename%)
-:no_error
-    time >NUL: /t
-goto :EOF
-
-:error_exit
     verify 2>NUL: other
+    setlocal EnableExtensions
+    if ErrorLevel 1 (
+	echo Error - Unable to enable extensions.
+	goto :EOF
+    )
+
+    for %%F in (cl_init.cmd) do if "" == "%%~$PATH:F" PATH %~dp0\cmd-lib.lib;%PATH%
+    call cl_init "%~f0" "%~1" || (echo Failed to initialise cmd-lib. & goto :exit)
+
+    if "%~1" == "/?"call cl_help  %~f0 & goto :error_exit
+    if "%~1" == ""  call cl_usage %~f0 & goto :error_exit
+
+    call cl_%0 %*
+    if defined _basename (echo %_basename%)
+    goto :exit
 goto :EOF
 
-rem vim: set filetype=dosbatch tabstop=8 softtabstop=4 shiftwidth=4 noexpandtab:
-rem vim: set foldmethod=indent
+rem ----------------------------------------------------------------------------
+rem Sets ErrorLevel and exit-status. Without a proper exit-status tests like
+rem 'command && echo Success || echo Failure' will not work,
+rem
+rem OBS: NO commands must follow the call to %ComSpec%, not even REM-arks,
+rem      or the exit-status will be destroyed. However, null commands like
+rem      labels (or ::) is okay.
+rem ----------------------------------------------------------------------------
+:no_error
+    time >NUL: /t	& rem Set ErrorLevel = 0.
+    goto :exit
+:error_exit
+    verify 2>NUL: other	& rem Set ErrorLevel = 1.
+:exit
+    %ComSpec% /c exit %ErrorLevel%
+
+:: vim: set filetype=dosbatch tabstop=8 softtabstop=4 shiftwidth=4 noexpandtab:
+:: vim: set foldmethod=indent
