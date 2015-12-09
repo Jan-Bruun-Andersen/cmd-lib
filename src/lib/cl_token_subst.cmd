859e05d4619bb88f09e778afc2730b72c5301d9e
Jan-Bruun-Andersen <jan_bruun_andersen@jabba.dk>
2015-12-09 05:25:21 +0100

    Fix problem with !-expansion.

--- src/lib/cl_token_subst.cmd
+++ src/lib/cl_token_subst.cmd
@@ -26,75 +26,115 @@
 :: =   | @echo off                                           |
 :: =   | call cl_token_subst "in.txt" "out.txt" DATE=%DATE%  |
 :: =   '-----------------------------------------------------'
-:: =
-:: = BUGS
-:: =   An unfortunate short-coming of cl_token_subst is that anything that
-:: =   looks like delayed-expansion variables (e.g. !variable!) will also be
-:: =   substituted/expanded. A work-around is to replace every ! with ^!.
 
 :: @author Jan Bruun Andersen
 :: @version @(#) Version: 2015-12-07
 
-    setlocal EnableDelayedExpansion
+    setlocal
     time >NUL: /t & rem Set ErrorLevel = 0.
 
     set "delims=@@"
     set "delim1="
     set "delim2="
 
-    if "%~1" == "/delims" set "delims=%~2" & shift & shift
+    if /i "%~1" == "/delims" set "delims=%~2" & shift /1 & shift /1
     set "delim1=%delims:~0,1%"
     set "delim2=%delims:~1,1%"
 
     if "%~1" == "" echo>&2 Error in function '%0'. Parameter 1 ^(in-file^) is null.	& goto :error_exit
     if "%~2" == "" echo>&2 Error in function '%0'. Parameter 2 ^(out-file^) is null.	& goto :error_exit
 
-    set "in_file=%~f1"  & shift
-    set "out_file=%~f1" & shift
-
-    for /L %%I in (1,1,6) do set "token%%I=" & set "value%%I="
-
-    if not "%~1" == "" set "token1=%1" & set "value1=%~2" & shift & shift
-    if not "%~1" == "" set "token2=%1" & set "value2=%~2" & shift & shift
-    if not "%~1" == "" set "token3=%1" & set "value3=%~2" & shift & shift
-    if not "%~1" == "" set "token4=%1" & set "value4=%~2" & shift & shift
-    if not "%~1" == "" set "token5=%1" & set "value5=%~2" & shift & shift
-    if not "%~1" == "" set "token6=%1" & set "value6=%~2" & shift & shift
-
-    if true == false (
-	echo delim1="%delim1%
-	echo delim2="%delim2%
-	echo.
-	if defined token1 echo token1="%token1%", value1="%value1%"
-	if defined token2 echo token2="%token2%", value2="%value2%"
-	if defined token3 echo token3="%token3%", value3="%value3%"
-	if defined token4 echo token4="%token4%", value4="%value4%"
-	if defined token5 echo token5="%token5%", value5="%value5%"
-	if defined token6 echo token6="%token6%", value6="%value6%"
-    )
+    set "in_file=%~1"  & shift /1
+    set "out_file=%~1" & shift /1
 
     if "%out_file%" == "%in_file%" (
-	echo Warning - Cowardly refuses to overwrite "%out_file%".
+	echo Warning - %0 cowardly refuses to overwrite the input-file "%in_file%".
 	goto :error_exit
+    )
+
+    rem Set tokens and values. Since we do not know how many token assignments
+    rem we have to process, we pass the original parameter list to ztokens.
+    rem ztokens is then responsible for ignoring any options, input and output
+    rem parameters, and only parse the token assignments.
+
+    call :ctokens
+    call :ztokens %*
+
+    if false == true (
+	echo delim1="%delim1%
+	echo delim2="%delim2%
+	for /L %%I in (1,1,100) do if defined token_%%I (
+	    call echo DEBUG token_%%I = '%%token_%%I%%', value_%%I = '%%value_%%I%%'
+	)
+	echo.
     )
 
     if exist "%out_file%" del "%out_file%"
 
+    rem The for-loop ignores and skips empty lines. 'findstr' can be used to add
+    rem a '<line-number>:' to every line of input, ensuring that we get a chance
+    rem to process it.
+
     for /F "usebackq delims= tokens=1" %%I in (`findstr /n /r ".*" "%in_file%"`) do (
-	set "S=I%%I"
-	if defined token1 set "S=!S:%delim1%%token1%%delim2%=%value1%!"
-	if defined token2 set "S=!S:%delim1%%token2%%delim2%=%value2%!"
-	if defined token3 set "S=!S:%delim1%%token3%%delim2%=%value3%!"
-	if defined token4 set "S=!S:%delim1%%token4%%delim2%=%value4%!"
-	if defined token5 set "S=!S:%delim1%%token5%%delim2%=%value5%!"
-	if defined token6 set "S=!S:%delim1%%token6%%delim2%=%value6%!"
-	set "S=!S:*:=!"
-	echo.!S!>>"%out_file%"
+	set "str=%%I"
+	call :subst
     )
 
     if not exist "%out_file%" goto :error_exit
     endlocal
     goto :exit
+goto :EOF
+
+rem ----------------------------------------------------------------------------
+rem Clears tokens and values.
+rem ----------------------------------------------------------------------------
+:ctokens
+    for /F %%V in ('set token_ 2^>NUL:') do set %%V=
+    for /F %%V in ('set value_ 2^>NUL:') do set %%V=
+goto :EOF
+
+rem ----------------------------------------------------------------------------
+rem Sets tokens and values.
+rem ----------------------------------------------------------------------------
+:ztokens [/delims XY] in-file out-file [token-assignment...]
+    rem Ignore /delims option and value.
+    if /i "%~1" == "/delims" shift & shift
+
+    rem Ignore in-file and out-file.
+    shift & shift
+
+    rem Parse remaining arguments as token assignments.
+    set n=1
+:z1 while
+    if "%~1" == "" goto :z2
+	set "token_%n%=%~1" & shift
+	set "value_%n%=%~1" & shift
+	set /a n+=1
+    goto :z1
+:z2
+    endlocal
+goto :EOF
+
+rem ----------------------------------------------------------------------------
+rem Performs token substititions.
+rem ----------------------------------------------------------------------------
+:subst
+    setlocal
+    rem Remove '<line-number>:' from input string.
+    set "str=%str:*:=%"
+
+    rem Start substituting tokens and values.
+    set n=1
+:s1 while
+    if not defined token_%n% goto :s2
+	call set "s1=%delim1%%%token_%n%%%%delim2%"
+	call set "s2=%%value_%n%%%"
+	call set "str=%%str:%s1%=%s2%%%"
+	echo.%str%>>"%out_file%"
+	set /a n+=1
+    goto :s1
+:s2
+    endlocal
 goto :EOF
 
 rem ----------------------------------------------------------------------------
